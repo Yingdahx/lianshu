a4bc85f0f4a5c5989ac6a555b4830e7392aaf638
hx_mac_new <2283203001@qq.com>
2018-10-31 15:26:37 +0800

    tab

--- lianshu/lianshu_app/management/commands/fifteen.py
+++ lianshu/lianshu_app/management/commands/fifteen.py
@@ -13,42 +13,42 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         pass
     def handle(self, *args, **options):
-    	now = datetime.datetime.now() #datetime
-	    print('-----post begin-----')
-	    print(now)
-	    now_tuple = int(time.mktime(now.timetuple()))                 #datetime->时间戳
-		last_tuple = now_tuple - 900                                  #15分钟前的时间戳   
+        now = datetime.datetime.now() #datetime
+        print('-----post begin-----')
+        print(now)
+        now_tuple = int(time.mktime(now.timetuple()))                 #datetime->时间戳
+        last_tuple = now_tuple - 900                                  #15分钟前的时间戳   
 
-	    headers = {
-	        "Content-Type": "application/json; charset=UTF-8",
-	        }
-	    url = "http://101.89.135.132/compressionstation/spillover/add/"
+        headers = {
+            "Content-Type": "application/json; charset=UTF-8",
+            }
+        url = "http://101.89.135.132/compressionstation/spillover/add/"
 
-	    #推送时间段内的数据
-	    resp = {}
-	    resp['res']= res = []
-	    stas = Frame_data.objects.filter(online_time__range=(last_tuple,now_tuple)).values_list('sta_id','manyi').distinct()
-	    stas = list(stas)
-	    for sta in stas:
-	        pyload = {}
-	        pyload['station'] = sta[0]
-	        pyload['spillover'] = sta[1]
-	        pyload['sensors'] = sensors = []
-	        datas = Frame_data.objects.filter(sta_id=sta[0],online_time__range=(last_tuple,now_tuple)).order_by('-online_time')
-	        for _ in datas:
-	            sensor = {}
-	            sensor['type'] = 0 #暂时默认置0
-	            sensor['status'] = _.status
-	            sensor['rawdata'] = _.data.dataFrame
-	            sensor['datatime'] = _.data.timestamp
-	            sensors.append(sensor)
-	        res.append(pyload)
+        #推送时间段内的数据
+        resp = {}
+        resp['res']= res = []
+        stas = Frame_data.objects.filter(online_time__range=(last_tuple,now_tuple)).values_list('sta_id','manyi').distinct()
+        stas = list(stas)
+        for sta in stas:
+            pyload = {}
+            pyload['station'] = sta[0]
+            pyload['spillover'] = sta[1]
+            pyload['sensors'] = sensors = []
+            datas = Frame_data.objects.filter(sta_id=sta[0],online_time__range=(last_tuple,now_tuple)).order_by('-online_time')
+            for _ in datas:
+                sensor = {}
+                sensor['type'] = 0 #暂时默认置0
+                sensor['status'] = _.status
+                sensor['rawdata'] = _.data.dataFrame
+                sensor['datatime'] = _.data.timestamp
+                sensors.append(sensor)
+            res.append(pyload)
 
-	    print('-----post data-----',resp)
-	    try :
-	    	response = requests.post(url, data=json.dumps(resp), headers=headers).text
-	    	print('-----post success-----',response)
-	    except:
-	    	response = requests.post(url, data=json.dumps(resp), headers=headers)
-	    	print('-----post failed-----',response)
-	    
+        print('-----post data-----',resp)
+        try :
+            response = requests.post(url, data=json.dumps(resp), headers=headers).text
+            print('-----post success-----',response)
+        except:
+            response = requests.post(url, data=json.dumps(resp), headers=headers)
+            print('-----post failed-----',response)
+        
