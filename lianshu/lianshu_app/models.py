1faa2d9a28fe275a26c323dbb3f9d36ecb6cbafa
hx_mac_new <2283203001@qq.com>
2018-11-03 09:48:17 +0800

    fcnt

--- lianshu/lianshu_app/models.py
+++ lianshu/lianshu_app/models.py
@@ -67,7 +67,7 @@ class Frame_data(models.Model):
 	decode_list = models.CharField(max_length=500,default='',verbose_name='解码字符串list')
 	sta_id = models.CharField(max_length=20,default='',verbose_name='小压站标识')
 	machine_id = models.CharField(max_length=200,default='',verbose_name='设备的EUI')
-	count = models.IntegerField(default=0,verbose_name='今天第几箱垃圾')
+	count = models.IntegerField(default=0,verbose_name='今天第几箱垃圾(暂用fcnt字段)')
 	manyi =  models.IntegerField(default=0,verbose_name='设备满溢参数')
 	action = models.IntegerField(default=0,verbose_name='垃圾翻斗动作次数')
 	status = models.IntegerField(default=0,verbose_name='是否在线')
