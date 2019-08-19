2db23ac456545c67550c8d926ca28aedb353c11b
mac <lijiang71>
2019-08-19 13:04:35 +0800

    lj

--- lianshu/lianshu_app/views.py
+++ lianshu/lianshu_app/views.py
@@ -57,7 +57,8 @@ def push(request):
 
     print('推送数据：',raw,sep='\n')
     #格林威治时间转为北京时区时间字串
-    timestr = timechange(raw['timestamp'])
+    # timestr = timechange(raw['timestamp'])
+    timestr = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     data = Push_data.objects.filter(deveui=raw['deveui']).first()
     if not data:
@@ -557,6 +558,89 @@ def get_xiaoyazhan(request):
         xiaoyazhan_list = paginator.page(paginator.num_pages)
 
     return render(request,'xiaoyazhan.html',locals())
+
+
+@csrf_exempt
+def bu_shu(request):
+    now = datetime.datetime.now() #datetime
+    print('-----'+str(now)+'-----')
+    print('-----post begin-----')
+    # logger.info('-----'+str(now)+'-----')
+    # logger.info('-----post begin-----')
+    now_tuple = int(time.mktime(now.timetuple()))                 #datetime->时间戳
+    last_tuple = now_tuple - 900                                  #15分钟前的时间戳   
+
+    headers = {
+        "Content-Type": "application/json; charset=UTF-8",
+        }
+    # url = "http://101.89.135.132/compressionstation/spillover/add/" #prod
+    url = "http://data.chi4rec.com.cn/compressionstation/spillover/add/"  #test
+
+    #推送时间段内的数据
+    res = []
+    get_bao_list = Bao_Wei.objects.filter(create_time__gt='2019-08-15 00:00')
+    for x in get_bao_list:
+        pyload = {}
+        if x.bw_input_txt:
+            if x.bw_input_txt:
+                raw = eval(x.bw_input_txt)
+
+                xianyazhan_status = Xiaoyazhan.objects.filter(data_id=int(raw['deveui'])).first()
+                if xianyazhan_status:
+                    pyload['status'] = xianyazhan_status.status
+                else:
+                    pyload['status'] = '维护表，不存在该小压站信息'
+
+        
+        print(pyload)
+    return JsonResponse({ 'success': True })
+
+
+    # stas = Frame_data.objects.filter(online_time__range=(last_tuple,now_tuple))
+    # for sta in stas:
+    #     pyload = {}
+
+    #     xianyazhan_status = Xiaoyazhan.objects.filter(data_id=sta.machine_id).first()
+    #     if xianyazhan_status:
+    #         pyload['status'] = xianyazhan_status.status
+    #     else:
+    #         pyload['status'] = '维护表，不存在该小压站信息'
+
+    #     pyload['station'] = sta.machine_id
+    #     pyload['spillover'] = sta.manyi
+    #     pyload['trunkNum'] = sta.count
+    #     pyload['operationNum'] = sta.action
+    #     pyload['refreshTime'] = sta.get_time
+    #     pyload['onlineTime'] = sta.online_time
+    #     #type字段暂时默认0
+    #     pyload['sensors'] = [{'type':0,'status':str(sta.status),'rawdata':sta.data.dataFrame,'datatime':sta.data.timestamp}]
+    #     res.append(pyload)
+
+    #     try:
+    #         Push_history.objects.create(data_id=sta.machine_id, manyi=sta.manyi, time_update=now, bw=pyload)
+    #     except Exception as e:
+    #         pass
+
+    # print('-----post data-----')
+    # print('-----post station num :'+str(len(res))+'-----')
+
+    # # logger.debug('-----post data-----')
+    # # logger.debug('-----post station:'+str(len(res)))
+
+    # try :
+    #     response = requests.post(url, data=json.dumps(res), headers=headers).text
+    #     if 'Success' in response:
+    #         pass
+    #     else:
+    #         Error.objects.create(error_id=now, error_address='推送不成功', error_bw=response)
+    #     print('-----post success-----'+response)
+    # except Exception as e:
+    #     print('-----post failed-----')
+    #     print('-----error-----')
+    #     print(e)
+    #     print('----------')
+    #     Error.objects.create(error_id=now, error_address='推送数据失败', error_bw=response)
+
 
 
 
