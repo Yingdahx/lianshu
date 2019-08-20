4bd02195ec83a16ad36e7d55ba1ed8f7f48c9397
mac <lijiang71>
2019-08-20 14:29:20 +0800

    lj

--- lianshu/lianshu_app/management/commands/bushu.py
+++ lianshu/lianshu_app/management/commands/bushu.py
@@ -17,70 +17,81 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         now = datetime.datetime.now() #datetime
         print('-----'+str(now)+'-----')
-        print('-----post begin-----')
-        # logger.info('-----'+str(now)+'-----')
-        # logger.info('-----post begin-----')
-        # now_tuple = int(time.mktime(now.timetuple()))                 #datetime->时间戳
-        # last_tuple = now_tuple - 900                                  #15分钟前的时间戳   
+        print('-----post begin-----') 
 
         headers = {
             "Content-Type": "application/json; charset=UTF-8",
             }
-        # url = "http://101.89.135.132/compressionstation/spillover/add/" #prod
         url = "http://data.chi4rec.com.cn/compressionstation/spillover/add/"  #test
 
+        res = []
+        i = 0
         #推送时间段内的数据
-        
-        get_bao_list = Bao_Wei.objects.filter(create_time__range=('2019-08-15 23:00','2019-08-19 00:00')).order_by('create_time')
-        for x in get_bao_list:
-            res = []
+        get_bao_list = Bao_Wei.objects.filter(create_time__range=('2019-08-16 20:30','2019-08-19 00:00')).order_by('create_time')
+        for x in get_bao_list: 
             pyload = {}
             if x.bw_input_txt:
-                if x.bw_input_txt:
-                    raw = eval(x.bw_input_txt)
-                    xianyazhan_status = Xiaoyazhan.objects.filter(data_id=int(raw['deveui'])).first()
-                    if xianyazhan_status:
-                        pyload['status'] = xianyazhan_status.status
-                    else:
-                        pyload['status'] = '维护表，不存在该小压站信息'
-
-                    #开始调用满溢度计算公式
-                    try:
-                        get_manyi_value = v.get_manyi(raw['deveui'])#计算满溢度
-                        
-                    except Exception as e:
-                        print('-----失败了-----')
-                        get_manyi_value = 0
-
-                    try:
-                        fandou = int('0x'+fram_list[5],16)#翻斗数
-                        get_manyi_value = v.find_manyidu_value(raw['deveui'],get_manyi_value,fandou)
-                    except Exception as e:
-                        get_manyi_value = 0
-                    
-
-                    fram_list = v.base64_decode(raw['dataFrame'])
-                    pyload['station'] = int(raw['deveui'])
-                    pyload['spillover'] = get_manyi_value
-                    pyload['trunkNum'] = raw['fcnt']
-                    pyload['operationNum'] = int('0x'+fram_list[5],16)
-                    pyload['refreshTime'] = time.mktime(x.create_time.timetuple())
-                    pyload['onlineTime'] = time.mktime(x.create_time.timetuple())
-                    pyload['sensors'] = [{'type':0,'status':str('1'),'rawdata':raw['dataFrame'],'datatime':x.create_time.strftime('%Y-%m-%d %H:%M:%S')}]
-                    res.append(pyload)
-            try :
-                response = requests.post(url, data=json.dumps(res), headers=headers).text
-                if 'Success' in response:
-                    pass
+                raw = eval(x.bw_input_txt)
+                xianyazhan_status = Xiaoyazhan.objects.filter(data_id=int(raw['deveui'])).first()
+                if xianyazhan_status:
+                    pyload['status'] = xianyazhan_status.status
                 else:
-                    Error.objects.create(error_id=now, error_address='推送不成功', error_bw=response)
-                print('-----post success-----'+response)
-            except Exception as e:
-                print('-----post failed-----')
-                print(e)
-                print('----------')
-                Error.objects.create(error_id=now, error_address='推送数据失败', error_bw=response)
-            print('==============>>>',str(x.create_time),'****',str(raw['deveui']))
-            Error.objects.create(error_id=now, error_address='补数据', 
-                error_bw=str(x.create_time)+ '=====' + str(raw['deveui']))
+                    pyload['status'] = '维护表，不存在该小压站信息'
 
+                #开始调用满溢度计算公式
+                try:
+                    get_manyi_value = v.get_manyi(raw['deveui'])#计算满溢度
+                    
+                except Exception as e:
+                    print('-----失败了-----')
+                    get_manyi_value = 0
+
+                try:
+                    fandou = int('0x'+fram_list[5],16)#翻斗数
+                    get_manyi_value = v.find_manyidu_value(raw['deveui'],get_manyi_value,fandou)
+                except Exception as e:
+                    get_manyi_value = 0
+                
+
+                fram_list = v.base64_decode(raw['dataFrame'])
+                pyload['station'] = int(raw['deveui'])
+                pyload['spillover'] = get_manyi_value
+                pyload['trunkNum'] = raw['fcnt']
+                pyload['operationNum'] = int('0x'+fram_list[5],16)
+                pyload['refreshTime'] = time.mktime(x.create_time.timetuple())
+                pyload['onlineTime'] = time.mktime(x.create_time.timetuple())
+                pyload['sensors'] = [{'type':0,'status':str('1'),'rawdata':raw['dataFrame'],'datatime':x.create_time.strftime('%Y-%m-%d %H:%M:%S')}]
+                res.append(pyload)
+                i += 1
+                if i == 100:
+                    try :
+                        response = requests.post(url, data=json.dumps(res), headers=headers).text
+                        if 'Success' in response:
+                            pass
+                        else:
+                            Error.objects.create(error_id=now, error_address='补数据不成功', error_bw=response)
+                        print('-----post success-----'+response)
+                    except Exception as e:
+                        print('-----post failed-----')
+                        print(e)
+                        Error.objects.create(error_id=now, error_address='补数据失败', error_bw=response)
+                    print('==============>>>',str(x.create_time),'****',str(raw['deveui']))
+                    Error.objects.create(error_id=now, error_address='补数据', 
+                        error_bw=str(x.create_time)+ '=====' + str(raw['deveui']))
+                    i = 0
+                    res = []
+
+        try :
+            response = requests.post(url, data=json.dumps(res), headers=headers).text
+            if 'Success' in response:
+                pass
+            else:
+                Error.objects.create(error_id=now, error_address='补数据不成功', error_bw=response)
+            print('-----post success-----'+response)
+        except Exception as e:
+            print('-----post failed-----')
+            print(e)
+            Error.objects.create(error_id=now, error_address='补数据失败', error_bw=response)
+        print('==============>>>',str(x.create_time),'****',str(raw['deveui']))
+        Error.objects.create(error_id=now, error_address='补数据', 
+            error_bw=str(x.create_time)+ '=====' + str(raw['deveui']))
--- lianshu/lianshu_app/views.py
+++ lianshu/lianshu_app/views.py
@@ -520,6 +520,7 @@ def find_manyidu_value(deveui,manyidu,fandou):
             )
     return manyidu
 
+    
 from django.core.paginator import Paginator
 
 @csrf_exempt
@@ -549,8 +550,6 @@ def get_xiaoyazhan(request):
                 get_update.save()
 
 
-
-
     paginator = Paginator(xiaoyazhan_list, pagesize) 
     try:
         xiaoyazhan_list = paginator.page(page)
@@ -558,130 +557,3 @@ def get_xiaoyazhan(request):
         xiaoyazhan_list = paginator.page(paginator.num_pages)
 
     return render(request,'xiaoyazhan.html',locals())
-
-
-# @csrf_exempt
-# def bu_shu(request):
-#     now = datetime.datetime.now() #datetime
-#     print('-----'+str(now)+'-----')
-#     print('-----post begin-----')
-#     # logger.info('-----'+str(now)+'-----')
-#     # logger.info('-----post begin-----')
-#     now_tuple = int(time.mktime(now.timetuple()))                 #datetime->时间戳
-#     last_tuple = now_tuple - 900                                  #15分钟前的时间戳   
-
-#     headers = {
-#         "Content-Type": "application/json; charset=UTF-8",
-#         }
-#     # url = "http://101.89.135.132/compressionstation/spillover/add/" #prod
-#     url = "http://data.chi4rec.com.cn/compressionstation/spillover/add/"  #test
-
-#     #推送时间段内的数据
-#     res = []
-#     get_bao_list = Bao_Wei.objects.filter(create_time__gt='2019-08-15 00:00')
-#     for x in get_bao_list:
-#         # print(x.create_time,'pppppppp')
-#         pyload = {}
-#         if x.bw_input_txt:
-#             if x.bw_input_txt:
-#                 raw = eval(x.bw_input_txt)
-#                 xianyazhan_status = Xiaoyazhan.objects.filter(data_id=int(raw['deveui'])).first()
-#                 if xianyazhan_status:
-#                     pyload['status'] = xianyazhan_status.status
-#                 else:
-#                     pyload['status'] = '维护表，不存在该小压站信息'
-
-#                 #开始调用满溢度计算公式
-#                 print('开始调用满溢度计算公式')
-#                 try:
-#                     get_manyi_value = get_manyi(raw['deveui'])#计算满溢度
-                    
-#                 except Exception as e:
-#                     print('满溢度计算出错')
-
-#                 print('满溢度计算完成')
-
-#                 print('判断满溢度是否变化')
-#                 try:
-#                     fandou = int('0x'+fram_list[5],16)#翻斗数
-#                     get_manyi_value = find_manyidu_value(raw['deveui'],get_manyi_value,fandou)
-#                 except Exception as e:
-#                     get_manyi_value = 0
-#                 print('判断结束')
-
-#                 fram_list = base64_decode(raw['dataFrame'])
-#                 pyload['station'] = int(raw['deveui'])
-#                 pyload['spillover'] = get_manyi_value
-#                 pyload['trunkNum'] = raw['fcnt']
-#                 pyload['operationNum'] = int('0x'+fram_list[5],16)
-#                 pyload['refreshTime'] = time.mktime(x.create_time.timetuple())
-#                 pyload['onlineTime'] = time.mktime(x.create_time.timetuple())
-#                 pyload['sensors'] = [{'type':0,'status':str('1'),'rawdata':raw['dataFrame'],'datatime':x.create_time.strftime('%Y-%m-%d %H:%M:%S')}]
-#                 res.append(pyload)
-        
-#     try :
-#         response = requests.post(url, data=json.dumps(res), headers=headers).text
-#         if 'Success' in response:
-#             pass
-#         else:
-#             Error.objects.create(error_id=now, error_address='推送不成功', error_bw=response)
-#         print('-----post success-----'+response)
-#     except Exception as e:
-#         print('-----post failed-----')
-#         print('-----error-----')
-#         print(e)
-#         print('----------')
-#         Error.objects.create(error_id=now, error_address='推送数据失败', error_bw=response)
-
-#     return JsonResponse({ 'success': True })
-
-
-    # stas = Frame_data.objects.filter(online_time__range=(last_tuple,now_tuple))
-    # for sta in stas:
-    #     pyload = {}
-
-    #     xianyazhan_status = Xiaoyazhan.objects.filter(data_id=sta.machine_id).first()
-    #     if xianyazhan_status:
-    #         pyload['status'] = xianyazhan_status.status
-    #     else:
-    #         pyload['status'] = '维护表，不存在该小压站信息'
-
-    #     pyload['station'] = sta.machine_id
-    #     pyload['spillover'] = sta.manyi
-    #     pyload['trunkNum'] = sta.count
-    #     pyload['operationNum'] = sta.action
-    #     pyload['refreshTime'] = sta.get_time
-    #     pyload['onlineTime'] = sta.online_time
-    #     #type字段暂时默认0
-    #     pyload['sensors'] = [{'type':0,'status':str(sta.status),'rawdata':sta.data.dataFrame,'datatime':sta.data.timestamp}]
-    #     res.append(pyload)
-
-    #     try:
-    #         Push_history.objects.create(data_id=sta.machine_id, manyi=sta.manyi, time_update=now, bw=pyload)
-    #     except Exception as e:
-    #         pass
-
-    # print('-----post data-----')
-    # print('-----post station num :'+str(len(res))+'-----')
-
-    # # logger.debug('-----post data-----')
-    # # logger.debug('-----post station:'+str(len(res)))
-
-    # try :
-    #     response = requests.post(url, data=json.dumps(res), headers=headers).text
-    #     if 'Success' in response:
-    #         pass
-    #     else:
-    #         Error.objects.create(error_id=now, error_address='推送不成功', error_bw=response)
-    #     print('-----post success-----'+response)
-    # except Exception as e:
-    #     print('-----post failed-----')
-    #     print('-----error-----')
-    #     print(e)
-    #     print('----------')
-    #     Error.objects.create(error_id=now, error_address='推送数据失败', error_bw=response)
-
-
-
-
-
